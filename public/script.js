e8a00cc75fc15d7a9dafa1d11fe5274bc06b4e2b
Kuldeep Dhaka <kdhaka94@gmail.com>
2020-12-08 13:36:59 +0530

    Added echo cancellation

--- public/script.js
+++ public/script.js
@@ -1,12 +1,19 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer()
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+})
+let userCounter = 0
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: true
+  audio: {
+    sampleSize:8,
+    echoCancellation: true
+  }
 }).then(stream => {
   addVideoStream(myVideo, stream)
 
@@ -20,11 +27,15 @@ navigator.mediaDevices.getUserMedia({
 
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
+    ++userCounter
+    console.log(userCounter)
   })
 })
 
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
+  --userCounter
+    console.log(userCounter)
 })
 
 myPeer.on('open', id => {
