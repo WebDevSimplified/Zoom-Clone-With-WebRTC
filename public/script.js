0b80ad5deba1ea997aa3ead83f1326788c8b19fa
Kuldeep Dhaka <kdhaka94@gmail.com>
2020-12-08 13:46:27 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
+const myPeer = new Peer()
 let userCounter = 0
 const myVideo = document.createElement('video')
 myVideo.muted = true
