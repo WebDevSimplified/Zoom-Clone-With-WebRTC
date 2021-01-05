648813c3a7d13f947549d5be26a03c45717396ee
jasenmichael <jasen@ready-builder.com>
2021-01-05 00:33:25 -0600

    feat: implement express to use peerServer

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+const myPeer = new Peer({
+	host: location.hostname,
+	port: location.port || (location.protocol === 'https:' ? 443 : 80),
+	path: '/peerjs'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
