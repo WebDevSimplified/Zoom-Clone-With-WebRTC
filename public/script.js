dd6ad41716b1b3ca0dec26b64f8cb421c9774966
cha wanghyeon <chawanghyeon@gmail.com>
2021-02-06 01:51:05 +0900

    apply coturn serveer

--- public/script.js
+++ public/script.js
@@ -1,56 +1,102 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
-const myVideo = document.createElement('video')
-myVideo.muted = true
-const peers = {}
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  addVideoStream(myVideo, stream)
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+let myPeer;
+alert(location.hostname)
+if(location.hostname === '192.168.35.115'){
+  myPeer = new Peer({
+    config: {
+      iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        {
+          urls: 'turn:110.8.201.37:3478?transport=tcp',
+          username: 'carking',
+          credential: 'carking',
+        },
+      ],
+    },
+    host: '192.168.35.115',
+    secure: true,
+    port: '3001',
+    debug: true,
+  });
+}else{
+  myPeer = new Peer({
+    config: {
+      iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        {
+          urls: 'turn:110.8.201.37:3478?transport=tcp',
+          username: 'carking',
+          credential: 'carking',
+        },
+      ],
+    },
+    host: '110.8.201.37',
+    secure: true,
+    port: '3001',
+    debug: true,
+  });
+}
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+const peers = {};
+let getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
 
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
-  })
+getUserMedia({ video: true, audio: true }, (stream) => {
+  addVideoStream(myVideo, stream);
+  socket.on('user-connected', (userId) => {
+    alert(userId);
+    console.log(userId, 'remoteid');
+    connectToNewUser(userId, stream);
+  });
+});
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
-})
+myPeer.on('call', (call) => {
+  getUserMedia({ video: true, audio: true }, function (stream) {
+    call.answer(stream);
+    const video = document.createElement('video');
+    call.on('stream', (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
+  });
+});
 
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
+socket.on('user-disconnected', (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
 
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
+myPeer.on('open', (id) => {
+  console.log(id, 'myid');
+  alert(id);
+  socket.emit('join-room', ROOM_ID, id);
+});
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
+  console.log(userId, stream);
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    console.log(userVideoStream);
+    addVideoStream(video, userVideoStream);
+  });
   call.on('close', () => {
-    video.remove()
-  })
+    video.remove();
+  });
 
-  peers[userId] = call
+  peers[userId] = call;
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
+  video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
+    video.play();
+  });
+  videoGrid.append(video);
 }
+
+myPeer.on('error', (e) => {
+  alert(e);
+});
